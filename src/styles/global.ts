34e4194fc80448d8240c385de6fad10a0f42673c
gilsonrusso <gilsonrusso@outlook.com>
2021-05-15 18:25:33 -0400

    add global style components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,16 +3,17 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
 
 :root {
-    --background: #f0f2f5;
-    --red: #e52e4d;
-    --blue: #5429cc;
-
-    --blue-light: #6933ff;
+  --background: #F0f2F5;
+  --red: #e52e4d;
+  --blue: #5429cc;
+  --green: #33CC95;
     
-    --text-title: #363f5f;
-    --text-body: #969cb3;
+  --blue-light: #6933ff;
+    
+  --text-title: #363f5f;
+  --text-body: #969cb3;
 
-    --shape: #FFFFFf;
+  --shape: #FFFFFF;
 }
 
 * {
@@ -22,35 +23,35 @@ export const GlobalStyle = createGlobalStyle`
 }
 
 html {
-    @media (max-width: 1080px) {
-        font-size: 93.75%;
-    }
-    @media (max-width: 720px) {
-        font-size: 87.5%;
-    }
+  @media (max-width: 1080px) {
+    font-size: 93.75%;
+  }
+  @media (max-width: 720px) {
+    font-size: 87.5%;
+  }
 }
 
 body {
-    background: var(---background);
-    -webkit-font-smoothing: antialiased;
+  background: var(--background);
+  -webkit-font-smoothing: antialiased;
 }
 
 body, input, textarea, button {
-    font-family: 'Poppins', sans-serif;
-    font-weight: 400;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
 }
 
 h1, h2, h3, h4, h5, h6, strong {
-    font-weight: 600;
+  font-weight: 600;
 }
 
 button {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 [disabled] {
-    opacity: 0.6;
-    cursor: not-allowed;
+  opacity: 0.6;
+  cursor: not-allowed;
 }
 
 `
